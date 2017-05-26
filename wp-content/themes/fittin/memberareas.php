81c5bc9b6ca9180bc7d29274fc6f89cef84305aa
Chris <chrispdunne@gmail.com>
2017-05-26 17:01:58 +0100

    videos - > all videos

--- wp-content/themes/fittin/memberareas.php
+++ wp-content/themes/fittin/memberareas.php
@@ -28,63 +28,63 @@
 
 
 	<?php woo_header_before(); ?>
-    
+
     <div class="member-nav" id="member-nav">
-    
+
          <img src="http://fittin.wpengine.com/wp-content/themes/fittin/images/member-icon.png" style="width: 25px; float: right;" id="member-nav-icon-close">
-        
+
         <ul>
             <li><a href=" https://fittin.wpengine.com/home-1/">Dashboard</a></li>
-            <li><a href="https://fittin.wpengine.com/videos/">Videos</a></li>
+            <li><a href="https://fittin.wpengine.com/videos/">All Videos</a></li>
             <li><a href="https://fittin.wpengine.com/myaccount/">My Account</a></li>
             <li><a href="https://fittin.wpengine.com/home-1/#stats">Statistics</a></li>
             <li><a href="https://fittin.wpengine.com/logout/">Logout</a></li>
         </ul>
-     
+
     </div>
 
 	<header id="header" class="member-head">
-        
+
         <div id="wrapper">
 
 	<div id="inner-wrapper">
-        
+
 
 		<a href="http://fittin.wpengine.com/" title="Online fitness tools for schools"><img src="http://fittin.wpengine.com/wp-content/uploads/2017/03/logo.png" alt="Fitt-In" width="100" style="float: left;"></a>
-        
+
         <div class="logo-strap">Movement breaks for mind and body</div>
-        
+
         <img src="http://fittin.wpengine.com/wp-content/themes/fittin/images/member-icon-white.png" style="width: 25px; float: right; margin-right: 30px; margin-top: 5px;" id="member-nav-icon">
 
 		<div style="clear:both;"></div>
 
 	</header>
 	<?php woo_header_after(); ?>
-            
+
             </div>
-        
+
         </div>
- 
+
 
     <div id="wrapper">
 
 	<div id="inner-wrapper">
-    
+
 <? global $woo_options;
-?>      
-    
+?>
+
     <!-- #content Starts -->
 	<?php woo_content_before(); ?>
     <div id="content" class="col-full">
-    
-    	<div id="main-sidebar-container">    
-            
-            
-		
+
+    	<div id="main-sidebar-container">
+
+
+
             <!-- #main Starts -->
             <?php woo_main_before(); ?>
             <section id="main" class="col-left">
-            	
+
 			<?php
                 if ( is_home() && is_active_sidebar( 'homepage' ) ) {
                     dynamic_sidebar( 'homepage' );
@@ -92,25 +92,25 @@
                     get_template_part( 'loop', 'index' );
                 }
             ?>
-                    
+
             </section><!-- /#main -->
             <?php woo_main_after(); ?>
-    
-            <?php get_sidebar(); ?>
-    
-		</div><!-- /#main-sidebar-container -->         
 
-		<?php get_sidebar( 'alt' ); ?>       
+            <?php get_sidebar(); ?>
+
+		</div><!-- /#main-sidebar-container -->
+
+		<?php get_sidebar( 'alt' ); ?>
 
     </div><!-- /#content -->
 	<?php woo_content_after(); ?>
-		
+
 <!-- FOOTER -->
-        
-        
-        
-        
-    
+
+
+
+
+
 
  <img src="http://fittin.wpengine.com/wp-content/themes/fittin/images/member-bg.png" style="width: 100%;">
 
@@ -123,10 +123,10 @@
 
 <?php wp_footer(); ?>
 <?php woo_foot(); ?>
-    
-    
+
+
 <script>
-   
+
     jQuery.noConflict();
     (function ($) {
         function readyFn() {
@@ -135,9 +135,9 @@
             });
         }
 
-        $(document).ready(readyFn); 
+        $(document).ready(readyFn);
     })(jQuery);
-    
+
      jQuery.noConflict();
     (function ($) {
         function readyFn() {
@@ -146,15 +146,13 @@
             });
         }
 
-        $(document).ready(readyFn); 
+        $(document).ready(readyFn);
     })(jQuery);
 
-</script>    
-    
-   
-    
-    
+</script>
+
+
+
+
 </body>
 </html>
-        
-        
