87861570d73e02678c1124fcd56bcc82833b690f
Chris <chrispdunne@gmail.com>
2017-06-29 19:33:43 +0100

    dynamically creating chart!!!

--- wp-content/themes/fittin/lib/ajax.php
+++ wp-content/themes/fittin/lib/ajax.php
@@ -6,46 +6,41 @@
 
 function ajax_handler() {
 
-	global $wpdb; // this is how you get access to the database
+	global $wpdb; // get access to the database
 
 	$user = intval( $_POST['user'] );
 	$video_duration = intval( $_POST['video_length'] );
 	$video_id = intval( $_POST['video_id'] );
+	$recent = get_user_meta( $user, 'time_list_most_recent', true );
+	$time_list = get_user_meta( $user, 'time_list', true );
 
-	$recent = get_user_meta( $user, 'time_list_most_recent' );
-
-	if ( '' != $recent[0] ) {
+	if ( '' != $recent ) {
 		// compare current time to recent time
-		$time_difference =  time() - $recent[0];
+		$time_difference =  time() - $recent[timestamp];
 
-		if ( $time_difference > 1 ) { // 5 mins
-			update_user_meta( $user, 'time_list_most_recent', array(
-					$video_id  => time()
-				)
-			);
-		} else {
+		if ( $time_difference < 1 ) { // 300 = 5 mins
 			die('not enough time elapsed');
 		}
-
-	} else { // no recent time found
-		update_user_meta( $user, 'time_list_most_recent', array(
-			'video_id' => $video_id,
-			'timestamp' => time()
-			)
-		);
 	}
 
-    $time_list = get_user_meta( $user, 'time_list'  );
+	// update recent time
+	update_user_meta( $user, 'time_list_most_recent', array(
+		'video_id' => $video_id,
+		'timestamp' => time()
+		)
+	);
 
-    if ( '' == $time_list ) {
-        $time_list = array();
-    }
+	// update time list
+	$day = date( 'd-m-Y', time() );
+	$time_list[$day][] = array(
+		'time' => time(),
+		'video_id' => $video_id,
+		'video_duration' => $video_duration
+	);
 
-    array_push( $time_list, array( 'video_id' => $video_id, 'timestamp' => time(), 'video_duration' => $video_duration ) );
+	update_user_meta( $user, 'time_list',  $time_list );
 
-    update_user_meta( $user, 'time_list', $time_list );
-
-    wp_die($recent);
+	wp_die();
 
 }
 add_action( 'wp_ajax_my_action', 'ajax_handler' );
