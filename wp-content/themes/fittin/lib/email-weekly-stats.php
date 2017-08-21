f2438b1af4091893cad638d4d11ae7712c60e9f3
Chris <chrispdunne@gmail.com>
2017-08-21 11:49:46 +0100

    email style

--- wp-content/themes/fittin/lib/email-weekly-stats.php
+++ wp-content/themes/fittin/lib/email-weekly-stats.php
@@ -32,7 +32,7 @@ $first_day = date( 'U', strtotime( 'last monday -9999 days' ) );
 $last_day = date('U');
 			$y = 0;
 
-			$output = "<img src='https://www.fitt-in.co.uk/wp-content/uploads/2017/03/logo.png' alt='Fitt-In'><p>Please find your video views for this week below: </p><table><tr><td style='border:1px solid #333'>Date</td><td style='border:1px solid #333'>Video views (mins)</td></tr>";
+			$output = "<img src='https://www.fitt-in.co.uk/wp-content/uploads/2017/03/logo.png' alt='Fitt-In'><p>Please find your video views for this week below: </p><table><tr><td style='font-weight: bold'>Date</td><td style='font-weight: bold'>Video views (mins)</td></tr>";
 			if ( !empty( $log ) ) {
 				foreach( $log as $key => $value ) {
 					$uni_key = strtotime($key);
@@ -45,7 +45,7 @@ $last_day = date('U');
 							$time += $entry['video_duration'];
 						}
 
-						$output .= "<tr><td style='border:1px solid #333'>". date( 'D jS F, Y', $uni_key ) . "</td><td>" . round( $time / 60 ) . " mins</td></tr>";
+						$output .= "<tr><td style='border:1px solid #333'>". date( 'D jS F, Y', $uni_key ) . "</td><td style='border:1px solid #333'>" . round( $time / 60 ) . " mins</td></tr>";
 
 					}
 					$y++;
